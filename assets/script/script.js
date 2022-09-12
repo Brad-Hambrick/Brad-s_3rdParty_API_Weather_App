9e4572a27bc19b7ff04ff85f9cf51c56d75797cb
Bradley Hambrick <bradleyhambrick@Bradleys-MacBook-Pro.local>
2022-09-11 23:34:40 -0500

    Worked on Icons and Search History

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,14 @@
+// API Weather Forecaster Javascript
 
-
-// let histBtn = document.getElementsByClassName('histBtn');
-
-
-
+var searchHistory = [];
+var searchHistoryContainer = document.querySelector("#history");
 
 function fetchWeather () {
     let location = document.getElementById('searchInput').value;
     let currentImage = document.getElementById('currentImage');
     let searchHist = document.getElementById('searchHist');
     let imageSource = "https://openweathermap.org/img/wn/";
+    let iconDescription = "Forecast weather conditions";
     console.log(location)
     let weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + location + ',US&units=imperial&appid=88d613e9a00e76b75b238a72808a570d'
     fetch(weatherUrl)          
@@ -17,22 +16,38 @@ function fetchWeather () {
     .then((data) => {
         console.log(data)
         document.getElementById('currentCity').innerHTML = location;
-        currentImage.innerHTML = imageSource + (data.weather[0].icon + ".png")
+        for (i=0; i<1; i++) {
+        var image = "https://openweathermap.org/img/wn/" + (data.weather[0].icon + ".png")
+        var weatherIcon = document.createElement("img");
+        imageContainer = document.querySelector("#currentImage")
+        weatherIcon.setAttribute('src', image);
+        weatherIcon.setAttribute('alt', iconDescription);
+        imageContainer.append(weatherIcon);
+        if (weatherIcon.length == i) {
+            weatherIcon.length -1;
+        }
+        }
         document.getElementById('currentTemp').innerHTML = "Current Temp: " + Number(data.main.temp) + "°"
         document.getElementById('currentWind').innerHTML = "Wind Speed: " + Number(data.wind.speed) + " MPH"
         document.getElementById('currentHumidity').innerHTML = "Humidity: " + Number(data.main.humidity) + " %"
         document.getElementById('currentUvIndex').innerHTML = "UV Index: " + Number(data.clouds.all)
 
-        for (let i=0; i<6; i++) {
-            let histBtn = $("<button></button>");
-    
-            if (i<6) {
-                $("div.searchHist").append(histBtn);
-                $(histBtn).addClass("histBtn");
-                i++;
+        searchHistory.push(location)
+        localStorage.setItem("search-history", JSON.stringify(searchHistory));
+        searchHistoryContainer.innerHTML = ""
+        for (let i=0; i<5; i++) {
+            let btn = document.createElement("button")
+            btn.setAttribute("type", "button")
+            btn.classList.add('history-btn', 'btn-history');
+            btn.setAttribute('data-search', searchHistory[i]);
+            btn.textContent = searchHistory[i];
+            searchHistoryContainer.append(btn);
+            btn.addEventListener = function () {
+                location=searchHistory
             }
+           
         }
-        document.getElementsByClassName('histBtn').innerHTML = location;
+        
         
 
     })
@@ -40,6 +55,7 @@ function fetchWeather () {
 
 function fetchForecast () {
     let location = document.getElementById('searchInput').value;
+    let iconDescription = "Forecast weather conditions"
     console.log(location)
     let weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + location + ",US&units=imperial&appid=88d613e9a00e76b75b238a72808a570d"
     fetch(weatherUrl)
@@ -50,8 +66,14 @@ function fetchForecast () {
             document.getElementById("forecastDate"+(i+1)).innerHTML = (data.list[i*8].dt_txt) 
         }
         for(i=0;i<5;i++){
-            document.getElementById("imageDay" + (i+1)).innerHTML = "https://openweathermap.org/img/wn/" + (data.list[i*8].weather[0].icon + ".png")
+            var image = "https://openweathermap.org/img/wn/" + (data.list[i*8].weather[0].icon) + ".png"
+            var weatherIcon = document.createElement("img");
+            imageContainer = document.querySelector("#imageDay" + (i+1))
+            weatherIcon.setAttribute('src', image);
+            weatherIcon.setAttribute('alt', iconDescription);
+            imageContainer.append(weatherIcon);
         }
+        
         for(i=0;i<5;i++){
             document.getElementById("tempDay"+(i+1)).innerHTML = "Temp: " + Number(data.list[i*8].main.temp) + "°"
         }
